be3691a936a3dbaea35f15bee20b1a56a572efe1
ikfront <qdaszx@naver.com>
2021-05-22 20:06:36 +0900

    Edit index.html main.css
    main section h1 수정하고 p태그에 있는 내용은 일단 삭제하고 메인페이지는 심플하게 갈게요!
    그리고 버튼에 있던 start-btn클래스는 삭제하고 관련 css도 삭제했어요
    
    Create typing.css typing.js
    메인페이지 심심하지않게 타이핑효과 넣었습니다
    
    추후에 글꼴 좀 더 이쁜 걸 찾아보죠

---
+++ js/typing.js
@@ -0,0 +1,18 @@
+const content = "Destiny 99 "
+const text = document.querySelector(".text")
+let index = 0;
+
+function typing() {
+  text.textContent += content[index++];
+  if (index == content.length) {
+    text.textContent = ""
+    index = 0;
+  }
+}
+
+let handle = setInterval(typing, 100)
+var repeat = setInterval(function () {
+  typing;
+  clearInterval(handle);
+}, 1000)
+
