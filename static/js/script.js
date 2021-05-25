68fc08f3b2afe2d1f81fe05e56860dbb0e533b38
ikfront <qdaszx@naver.com>
2021-05-25 15:22:47 +0900

    Edit endPoint

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
-const endPoint = 15;
+const endPoint = 14;
 
 function goResult() {
   qna.style.WebkitAnimation = "fadeOut 1s";
