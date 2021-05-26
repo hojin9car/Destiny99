1da44a8824e84efb622a552d950865beceea5bbb
ikfront <qdaszx@naver.com>
2021-05-26 17:03:54 +0900

    typing 수정

--- static/js/typing.js
+++ static/js/typing.js
@@ -1,17 +1,17 @@
-const content = "Destiny 99 "
+const content = "Destiny99 "
 const text = document.querySelector(".text")
 let index = 0;
 
 function typing() {
   text.textContent += content[index++];
   if (index == content.length) {
-    text.textContent = "D"
+    text.textContent = "D";
     index = 1;
   }
 }
 
-let handle = setInterval(typing, 100)
+let handle = setInterval(typing, 1000)
 var repeat = setInterval(function () {
   typing();
   clearInterval(handle);
-}, 1000)
+}, 600)
